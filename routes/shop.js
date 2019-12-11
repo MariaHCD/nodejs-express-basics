20d7855e5d2f83cc9a3f06419e135eab225d2226
MariaHCD <mariahcdcosta@gmail.com>
2019-12-11 22:19:58 +0400

    Export data from admin module

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../util/path');
+const adminModule = require('./admin');
 
 router.get('/', (req, res, next) => {
+    console.log(adminModule.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
