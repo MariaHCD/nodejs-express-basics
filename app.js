8788de49d3b84836f59a7c56369bbd64319f0670
MariaHCD <mariahcdcosta@gmail.com>
2020-01-18 20:24:54 +0400

    Adjustments to styling

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
@@ -12,6 +9,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
-const express = require('express');
-
 const path = require('path');
 
-const router = express.Router();
+const express = require('express');
 
 const adminController = require('../controllers/admin');
 
-router.get('/products', adminController.getProducts);
+const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
